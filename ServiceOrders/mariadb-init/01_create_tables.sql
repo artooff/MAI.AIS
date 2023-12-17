9e353bfa168b00f995743c081d55f6ebb9a46369
artooff <artov200270@gmail.com>
2023-12-17 19:33:28 +0300

    update

--- ServiceOrders/mariadb-init/01_create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Users (
-    Id INT PRIMARY KEY,
-    Login VARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    FirstName VARCHAR(255),
-    LastName VARCHAR(255),
-    Email VARCHAR(255),
-    Treatment INT
-);
